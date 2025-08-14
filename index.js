de99353394ee24ab2d0ea480f7957e60efd95937
pavelren-pr <pavelren.pr@gmail.com>
2025-08-14 22:15:08 +0300

    14/08/2025

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config(); // Загружаем переменные из .env
-const { Telegraf } = require('telegraf');
+const { Telegraf,
+  Markup
+ } = require('telegraf');
 const { message } = require('telegraf/filters');
 
 // Проверяем, что токен есть
@@ -11,6 +13,31 @@ bot.start((ctx) => ctx.reply(`Добро пожаловать! ${ctx.message.fro
 bot.help((ctx) => ctx.reply('Отправьте мне стикер'));
 bot.on(message('sticker'), (ctx) => ctx.reply('❤️'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.command('go', (ctx) => {
+  try {
+  ctx.replyWithHTML('<b>Выберите действие</b>', Markup.inlineKeyboard(
+    [
+      [Markup.button.callback('Написать в группу', 'btn_1')], 
+      [Markup.button.callback('Отправить сообщение админу', 'btn_2')],
+      [Markup.button.callback('Показать информацию', 'btn_3')], 
+      [Markup.button.callback('Отправить скриншот', 'btn_4')],
+    ]))
+  } catch(e) {
+    console.error('Ошибка при обработке команды /go:', e);
+    ctx.reply('Произошла ошибка при обработке вашей команды. Пожалуйста, попробуйте позже.');
+  }
+  })
+
+bot.action('btn_1', async (ctx) => {
+  try {
+    await ctx.answerCbQuery();
+    await ctx.replyWithDice({ emoji: '🎲' });
+  } catch (e) {
+    console.error('Ошибка при обработке кнопки btn_1:', e);
+    ctx.reply('Произошла ошибка при обработке вашей команды. Пожалуйста, попробуйте позже.');
+  }
+})
 
 bot.launch()
   .then(() => console.log("🤖 Бот запущен!"))
